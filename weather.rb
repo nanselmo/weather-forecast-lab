6655fa24007a041de9c002979ccddf1309076ade
Nicki Anselmo <nickianselmo@gmail.com>
2017-07-25 13:23:10 -0500

    named each methods so rspec works

--- weather.rb
+++ weather.rb
@@ -14,15 +14,17 @@ end
 # 2. Using the location method as a guide, write a  method called "description" so it returns the description from the weather hash. 
 
 
-# 3. Using the methods above and string interpolation, update the method below so that it returns a string that describes the current weather - for example: "The weather in New York is Partly Cloudy. The temperature is 66.1 degrees Farenheit." HINT: the method should RETURN the string, not print it to the terminal. 
+# 3. Using the methods above and string interpolation, update the weather_report method below so that it returns a string that describes the current weather - for example: "The weather in New York is Partly Cloudy. The temperature is 66.1 degrees Farenheit." HINT: the method should RETURN the string, not print it to the terminal. 
+def weather_report(weather_hash)
+end	
+	
 
-
-#4. Write a method that takes in the weather hash as an argument. 
+#4. Write a method city_weather_info that takes in the weather hash as an argument. 
 # You'll want to use the each method to iterate over the hash and print out
 # `The city is New York.` `The description is Partly Cloudy.` `The temperature is 66.3.`
 
 
-# BONUS:  Write a method that takes in the weather hash as an argument and converts the farenheit temperature to celcius. Have the method round your answer to two decimal places. Is there a method we can use to round in Ruby?
+# BONUS:  Write a method, convert_temp, that takes in the weather hash as an argument and converts the farenheit temperature to celcius. Have the method round your answer to two decimal places. Is there a method we can use to round in Ruby?
 
 # Hint - how can we find the algorithm to convert Farenheit to Celsius?
 
